167ba000b22042563f2f713bb5e3fcde02c615ed
surajlifeean <surajjeswara@yahoo.com>
2018-02-17 21:15:28 +0530

    flipkart data scrapped

--- flipkart/flipkart.py
+++ flipkart/flipkart.py
@@ -1,14 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 url = "https://www.flipkart.com/womens-clothing/western-wear/pr?sid=2oq,c1r,ha6&otracker=categorytree&otracker=nmenu_sub_Women_0_Western%20Wear"
 headers = {'User-Agent':'Mozilla/5.0'}
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
 all_divs=soup.find("div",class_='GQtpzo')
 i=0
-A=[]
-B=[]
 for div in all_divs:
     #name=a.find("a",class_='_2cLu-l')
     if(i==2):
@@ -17,13 +14,8 @@ for div in all_divs:
             #print(pro)
             product=pro.find("a",class_='_2cLu-l')
             price=pro.find("div",class_='_1vC4OE')
-            #print(product.text)
-            #print(price.text)
-            A.append(product.text)
-            B.append(price.text)
+            print(product.text)
+            print(price.text)
+            print('-------------------------------------------------------------------------------------------------------------------------------',j)
             j=j+1
     i=i+1
-df=pd.DataFrame(A,columns=['Product'])
-df['Price']=B
-
-print(df)
